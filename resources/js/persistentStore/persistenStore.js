69cb7b6ef14d462fce188d7d7249482fdcb6c42a
AndeeDev <andrey14501450@gmail.com>
2020-09-16 13:24:09 +0300

    fix: persistent store implementation

---
+++ resources/js/persistentStore/persistenStore.js
@@ -0,0 +1,29 @@
+
+const persistenStore = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if(serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (e) {
+        return undefined;
+    }
+}
+
+const saveState = (state) => {
+    try {
+        const serializeState = JSON.stringify(state);
+        localStorage.setItem('state', serializeState);
+    } catch (e) {
+
+    }
+}
+const clearStorage = () => {
+    try{
+        localStorage.clear();
+    } catch (e) {
+
+    }
+}
+export {persistenStore, saveState, clearStorage};
