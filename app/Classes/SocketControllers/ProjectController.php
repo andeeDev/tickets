05024ffd68859241b1b695bc657d4c001b338e28
AndeeDev <andrey14501450@gmail.com>
2020-10-21 21:30:42 +0300

    fix: added transactions and error handling

--- app/Classes/SocketControllers/ProjectController.php
+++ app/Classes/SocketControllers/ProjectController.php
@@ -4,27 +4,70 @@
 namespace App\Classes\SocketControllers;
 
 
+use App\Classes\Services\ProjectService;
 use App\Project;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 
 class ProjectController
 {
-    public function updateTitle($message, $send) {
-        $project = Project::find(intval($message["id"]));
-        $project->title = $message["title"];
-        $project->save();
-        $send($project);
+    private $service;
+
+    public function __construct()
+    {
+        $this->service = new ProjectService();
     }
 
-    public function deleteProject($message, $send) {
-        $obj = [];
-        $obj["type"] = "projects.delete";
-        $project_id = intval($message["id"]);
-        $obj["project_id"] = $project_id;
-        $project = Project::find($project_id);
-        $project->tickets()->delete();
-        Project::destroy($project_id);
-        $send(json_encode($obj));
+    public function updateTitle($message, $send, $responseWithError) {
+        $validator = Validator::make($message, [
+            'id' => 'required|integer',
+            'title' => 'required|string|max:50'
+        ]);
+        if ($validator->fails()) {
+            Log::channel('errorLogger')->error('Update title, transaction error', ['errors' => $validator->errors()]);
+            $responseWithError(json_encode(["error" => "Data is not valid"]));
+        }
+        DB::beginTransaction();
+        try {
+            print_r($message["title"]);
+            $project = $this->service->updateProjectTitle($message["id"], $message["title"]);
+            DB::commit();
+            $send($project);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            Log::channel('errorLogger')->error('Update title, transaction error' .  $exception->getMessage());
+            $responseWithError(json_encode(["error" => "Can't update title" . $exception->getMessage()]));
+        }
+    }
+
+    public function deleteProject($message, $send, $responseWithError) {
+        $validator = Validator::make($message, [
+            'id' => 'required|integer'
+        ]);
+        if ($validator->fails()) {
+            Log::channel('errorLogger')->error('Update title, transaction error', ['errors' => $validator->errors()]);
+            $responseWithError(json_encode(["error" => "Data is not valid"]));
+        }
+        DB::beginTransaction();
+        try {
+            $obj = [];
+            $obj["type"] = "projects.delete";
+            $project_id = intval($message["id"]);
+            $obj["project_id"] = $project_id;
+            $project = Project::find($project_id);
+            $project->tickets()->delete();
+            Project::destroy($project_id);
+            DB::commit();
+            $send(json_encode($obj));
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            Log::channel('errorLogger')->error('Delete project, transaction error');
+            $responseWithError(json_encode(["error" => "Can't delete project" ]));
+        }
+
     }
 
 }
--- app/Classes/SocketControllers/TicketsController.php
+++ app/Classes/SocketControllers/TicketsController.php
@@ -3,19 +3,22 @@
 
 namespace App\Classes\SocketControllers;
 
-
-
-use App\Classes\ApiTokenCookieFactory;
-use App\OauthAccessToken;
-use App\Project;
+use App\Classes\Services\TicketService;
 use App\Ticket;
-use App\User;
-use Firebase\JWT\JWT;
-use Illuminate\Auth\AuthenticationException;
-use mysql_xdevapi\Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
 
 class TicketsController
 {
+
+    private $service;
+    public function __construct()
+    {
+        $this->service = new TicketService();
+    }
+
     public function getUserId($accessToken) {
         try {
             $client = new \GuzzleHttp\Client;
@@ -34,111 +37,101 @@ class TicketsController
 
 
 
-    public function create($message, $send) {
-        $ticket = new Ticket();
-        if($user_id = $this->getUserId($message["access_token"])){
-            $ticket->save();
-            $ticket->users()->attach($user_id, ['is_master' => 1]);
-            $id = $ticket["id"];
+    public function create($message, $send, $responseWithError) {
+        $validator = Validator::make($message, [
+            'access_token' => 'required|string'
+        ]);
+        if ($validator->fails()) {
+            Log::channel('errorLogger')->error('Create ticket, validation error', ['errors' => $validator->errors()]);
+            $responseWithError(json_encode(["error" => "Data is not valid"]));
+        }
+        DB::beginTransaction();
+        try {
+            $ticket = new Ticket();
+            if($user_id = $this->getUserId($message["access_token"])){
+                $ticket->save();
+                $ticket->users()->attach($user_id, ['is_master' => 1]);
+                $id = $ticket["id"];
+                $jsonObject = [];
+                $jsonObject["type"] = "tickets.add";
+                $jsonObject["ticket"] = Ticket::with(['projects' => function($query) {
+                    $query->select('id', 'title');
+                }, 'users' => function($query) {
+                    $query->select('id', 'name', 'email', 'is_master', 'image');
+                }])->find($id);
+                DB::commit();
+                //creator
+                $send(json_encode($jsonObject));
+            }
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            Log::channel('errorLogger')->error('Create Ticket, transaction error');
+            $responseWithError(json_encode(["error" => "Can't create ticket" ]));
+        }
+    }
+
+    public function delete($message, $send, $responseWithError) {
+        $validator = Validator::make($message, [
+            'access_token' => 'required|string'
+        ]);
+        if ($validator->fails()) {
+            Log::channel('errorLogger')->error('Delete ticket, validation error', ['errors' => $validator->errors()]);
+            $responseWithError(json_encode(["error" => "Data is not valid"]));
+        }
+        DB::beginTransaction();
+        try {
+            $ticket = Ticket::find($message["id"]);
+            $id = $message["id"];
+            $ticket->users()->detach();
+            $ticket->destroy($id);
             $jsonObject = [];
-            $jsonObject["type"] = "tickets.add";
-            $jsonObject["ticket"] = Ticket::with(['projects' => function($query) {
-                $query->select('id', 'title');
-            }, 'users' => function($query) {
-                $query->select('id', 'name', 'email', 'is_master', 'image');
-            }])->find($id);
-            //creator
+            $jsonObject["type"] = "tickets.delete";
+            $jsonObject["ticket"] = $ticket;
+            DB::commit();
             $send(json_encode($jsonObject));
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            Log::channel('errorLogger')->error('Delete Ticket, transaction error' . $exception->getMessage());
+            $responseWithError(json_encode(["error" => "Can't delete ticket" ]));
         }
-
 
     }
 
-    public function delete($message, $send) {
-        $ticket = Ticket::find($message["id"]);
-        $id = $message["id"];
-        $ticket->users()->detach();
-        $ticket->destroy($id);
-        $jsonObject = [];
-        $jsonObject["type"] = "tickets.delete";
-        $jsonObject["ticket"] = $ticket;
-        $send(json_encode($jsonObject));
+    public function update($message, $send, $responseWithError) {
+        $validator = Validator::make($message, [
+            'access_token' => 'required|string',
+            'ticket' => 'required|array',
+            'ticket.id' => 'required|integer',
+            'ticket.title' => 'nullable|string|max:50',
+            'ticket.subject' => 'nullable|string|max:50',
+            'ticket.description' => 'nullable|string|max:50',
+            'ticket.status' => 'nullable|in:Done,In Progress,New',
+            'ticket.priority' => 'nullable|in:High,Middle,Low',
+            'ticket.users.id' => 'exists:users,id',
+            'ticket.users.name' => 'nullable|string',
+            'ticket.users.email' => 'nullable|string|email',
+            'ticket.users.is_master' => 'nullable|integer|in:1,0',
+            'ticket.users.image' => 'nullable'
+        ]);
+        if ($validator->fails()) {
+            Log::channel('errorLogger')->error('Update ticket, validation error', ['errors' => $validator->errors()]);
+            $responseWithError(json_encode(["error" => "Data is not valid"]));
+        }
+        DB::beginTransaction();
+        try {
+            $ticket_to_send = $this->service->updateTicket($message["ticket"], $message["access_token"]);
+            $jsonObject = [];
+            $jsonObject["type"] = "tickets.update";
+            $jsonObject["ticket"] = $ticket_to_send;
+            DB::commit();
+            $send(json_encode($jsonObject));
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            Log::channel('errorLogger')->error('Update Ticket, transaction error' . $exception->getMessage());
+            $responseWithError(json_encode(["error" => "Can't Update ticket" ]));
+        }
+
     }
 
-    public function update($message, $send) {
-        $ticketArray = $message["ticket"];
-        //create project
-        if(array_key_exists("project_name", $ticketArray)) {
-            $project = Project::where('title', '=', $ticketArray["project_name"])->first();
-            if(!$project) {
-                $that_project = Project::create([
-                    'title' => $ticketArray["project_name"],
-                    'user_id' => $this->getUserId($message["access_token"])
-                ]);
-                $ticketArray["project_id"] = $that_project->id;
-            } else {
-                $ticketArray["project_id"] = $project->id;
-            }
-            unset($ticketArray["project_name"]);
-            unset($ticketArray["projects"]);
-        }
-        unset($ticketArray["projects"]);
-        $ticket = Ticket::find($ticketArray["id"]);
-        unset($ticketArray["id"]);
-        $ticketArray["priority"] = $this->convert_priority($ticketArray["priority"]);
-        $ticketArray["status"] = $this->convert_status($ticketArray["status"]);
-        $creatorId = null;
-        $assignerId = null;
 
-        foreach($ticketArray["users"] as $user) {
-            if (intval($user["is_master"]) === 1) {
-                $creatorId = User::select("id")->where("email", $user["email"])->first()->id;
-            }
-            if (intval($user["is_master"]) === 0 && array_key_exists("email", $user)) {
-                $assignerId = User::select("id")->where("email", $user["email"])->first()->id;
-            }
-        }
-        $ticket->users()->detach();
-        $ticket->users()->attach([$creatorId => ["is_master" => '1']]);
-        if($assignerId) {
-            $ticket->users()->attach([$assignerId => ["is_master" => '0']]);
-        }
-        /*$ticket->save();*/
-        $ticket->update($ticketArray);
-        /*$ticket->save();*/
-        $jsonObject = [];
-        $jsonObject["type"] = "tickets.update";
-        $ticket_to_send = $ticket->with(['projects' => function($query) {
-            $query->select('id', 'title');
-        }, 'users' => function($query) {
-            $query->select('id', 'name', 'email', 'is_master', 'image');
-        }])->find($ticket->id);
-        $jsonObject["ticket"] = $ticket_to_send;
-        $send(json_encode($jsonObject));
-    }
-
-    function convert_priority($priority) {
-        switch ($priority) {
-            case "High":
-                return "high";
-            case "Middle":
-                return "normal";
-            case "Low":
-                return "low";
-            default:
-                return "";
-        }
-    }
-    function convert_status($status) {
-        switch ($status) {
-            case "New":
-                return "new";
-            case "In Progress":
-                return "progress";
-            case "Done":
-                return "done";
-            default:
-                    return "";
-        }
-    }
 }
